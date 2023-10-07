98bef34e56d10d4849f51188a8443ecace19e22b
rabayakhatun <rabayakhatun1726@gmail.com>
2023-10-07 20:07:00 +0300

    Initial commit

---
+++ src/Myprintapp.java
@@ -0,0 +1,54 @@
+
+public class Myprintapp {
+
+		    public static void main(String[] args) {
+		        // create project name.
+		        System.out.println("Printer App");
+
+	 
+		        System.out.print("This app is made by: ");
+
+		       
+		        String creator = "keya";
+
+		        // Print creator name.
+		        System.out.println(creator);
+
+		        // Declare two variable.
+		        int number1 = 5;
+		        int number2 = 2;
+		        
+		       // System.out.println(number1);
+                  
+		        
+		        System.out.println("The value of number1 is " + number1);
+
+		        // 
+		        System.out.println("The value of number2 is " + number2);
+
+		        //  Declare an integer typed variable.
+		        int multiplication;
+
+		        // 
+		        multiplication = number1 * number2;
+
+		        //  
+		        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+
+		        //  Doing addition, division, and subtraction(change integer to double for division).
+		        int addition = number1 + number2;
+		        double division = (double) number1 / number2;
+		        int subtraction = number1 - number2;
+
+		        System.out.println(number1 + " + " + number2 + " = " + addition);
+		        System.out.println(number1 + " / " + number2 + " = " + division);
+		        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+
+		        
+		    }
+		
+
+
+	}
+
+
